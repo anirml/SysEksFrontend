7c6134299987d6d2d976c6e26eb07b2ba3f00fb0
Ulrik Holm <ulrikholmuh@gmail.com>
2019-11-20 19:58:58 +0100

    table2

--- src/App.js
+++ src/App.js
@@ -7,32 +7,55 @@ const flights = [{id: 1, date: 'Dec 1, 2019 8:50:00 PM', duration: 60000, price:
 {id: 2, date: 'Dec 1, 2019 6:50:00 PM', duration: 70000, price: 122.0, origin: 'CPH', destination: 'LHR', link: 'http://beesbeesbees.com/' },
 {id: 3, date: 'Dec 1, 2019 7:20:00 PM', duration: 45000, price: 95.0, origin: 'CPH', destination: 'LHR', link: 'https://thatsthefinger.com/'}]
 
+
+function sorter(sortBy){
+  var key = sortBy;
+      function compare(a, b) {
+          const comA = a[key];
+          const comB = b[key];
+          let comparison = 0;
+          if (comA > comB) {
+          comparison = 1;
+          } else if (comA < comB) {
+          comparison = -1;
+          }
+          return comparison;
+      }
+return compare;
+}
+
+function priceflight(array) {return flights.sort(sorter("price"))}
+
   return (
     <div className="App">
-      <h1>JJUGroup</h1>
-      <table border="1">
+      <h1>Memeondo</h1>
+      <table className="table table-striped">
+      <thead>
         <tr>
-          <th>ID</th>
-          <th>Date</th>
-          <th>Duration</th>
-          <th>Price</th>
-          <th>Origin</th>
-          <th>Destination</th>
-          <th>Link</th>
+          <th scope="col">ID</th>
+          <th scope="col">Date</th>
+          <th scope="col">Duration</th>
+          <th scope="col">Price</th>
+          <th scope="col">Origin</th>
+          <th scope="col">Destination</th>
+          <th scope="col">Link</th>
         </tr>
+        </thead>
+        <tbody>
         {flights.map((f)=>{
-          return <tr><td>{f.id}</td>
+          return <tr><th scope="row">{f.id}</th>
           <td>{f.date}</td>
           <td>{f.duration}</td>
           <td>${f.price}</td>
           <td>{f.origin}</td>
           <td>{f.destination}</td>
-          <td><a href={f.link}>Link</a></td>
-          
+          <td><a className="btn btn-success" href={f.link}>Se tilbud</a></td>
           </tr>
         })}
-
+        </tbody>
       </table>
+      <p>{console.log(flights.sort(sorter('duration')))}</p>
+      
     </div>
   );
 }
