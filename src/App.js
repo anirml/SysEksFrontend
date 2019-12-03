404531957851e241b46df5630a437255b7b8b861
Epiko1994 <cph-jj442@cphbusiness.dk>
2019-12-03 11:01:11 +0100

    merged and deleted commented code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-//import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import logo from './AVINATION_3-01.svg';
 import FB from './Facebook_Logo.svg';
@@ -9,11 +8,6 @@ import Insta from './Instagram_Logo.svg';
 function App() {
 
   const url = 'https://jjugroup.ga/SysEksBackend/api/flight/combined'
-
-  // const loadFlights = () => {
-  //   return fetch(url)
-  //     .then(res => { return res.json() })
-  // }
 
   const GetAllFlight = () => {
     fetch(url)
@@ -35,17 +29,6 @@ function App() {
   { id: 2, date: 'Dec 1, 2019 6:50:00 PM', duration: 70000, price: 122.0, origin: 'CPH', destination: 'LHR', link: 'http://beesbeesbees.com/' },
   { id: 3, date: 'Dec 1, 2019 7:20:00 PM', duration: 45000, price: 95.0, origin: 'CPH', destination: 'LHR', link: 'https://thatsthefinger.com/' }]
 
-  // const getFlights = () =>{
-  //   fetch('http://localhost:8080/jjugroup/api/flight/all')
-  //     .then(function(response) {
-  //       return response.json();
-  //     })
-  //     .then(function(data) {
-  //       console.log(data);
-  //     })
-  //     .then((data) =>{setFlights(data)});
-  // }
-
   function sorter(sortBy) {
     var key = sortBy;
     function compare(a, b) {
@@ -61,8 +44,6 @@ function App() {
     }
     return compare;
   }
-
-  // const flightKeys = Object.keys(flights[1]);
 
   const milliToHourMin = (milliseconds) => {
     let sec = milliseconds/1000
@@ -97,42 +78,6 @@ function App() {
   }
 
   const [table, setTable] = useState(flights)
-
-  // return (
-  //   <div className="App">
-  //     <h1>Memeondo</h1>
-  //     <br />
-  //     <h5>Sort by:</h5>
-  //     <div className="btn-group" role="group" aria-label="Basic example">
-  //     {flightKeys.slice(0,flightKeys.length-1).map((fk)=>{return <button key={fk} type="button" className="btn btn-outline-secondary btn-lg" onClick={() => setTable(mapper(flights.sort(sorter(fk))))}>{fk.charAt(0).toUpperCase() + fk.slice(1)}</button> })}
-  //     </div>
-  //     <br/>
-  //     <loadFlights/>
-  //     <br/>
-  //  <table className="table table-striped">
-  //   <thead>
-  //     <tr>
-  //       <th scope="col">ID</th>
-  //       <th scope="col">Date</th>
-  //       <th scope="col">Duration</th>
-  //       <th scope="col">Price</th>
-  //       <th scope="col">Origin</th>
-  //       <th scope="col">Destination</th>
-  //       <th scope="col">Link</th>
-  //     </tr>
-  //   </thead>
-  //   <tbody>
-  //      {table}
-  //   </tbody>
-  // </table>
-  //     {/* <button onClick={() => setTable(mapper(flights.sort(sorter('price'))))}>pris</button>
-  //     <button onClick={() => setTable(mapper(flights.sort(sorter('duration'))))}>duration</button>
-  //     <button onClick={() => setTable(mapper(flights.sort(sorter('id'))))}>id</button>
-  //     <button onClick={() => setTable(mapper(flights.sort(sorter('date'))))}>date</button>
-  //     <button onClick={() => console.log(flightKeys.map((fk)=>{return <p>{fk + 1}</p> }))}>lalala</button> */}
-
-  //   </div>
-  // );
 
   const SearchFlights = (urlend) =>{
     fetch('https://jjugroup.ga/SysEksBackend/api/flight/' + urlend)
